f74505ecaede74659b9ffe258cecbda4ed39e7b6
Maria Eduarda Amaral <44277668+MariaAmaralSB@users.noreply.github.com>
2021-10-08 15:14:52 -0300

    Adding the project

---
+++ UI/UI/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿
+using UI.Core;
+
+namespace UI.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        public HomeViewModel HomeVm { get; set; }
+
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+            CurrentView = HomeVm;
+
+
+            HomeViewCommand = new RelayCommand(o => 
+            {
+                CurrentView = HomeVm;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVM;
+            });
+        }
+
+    
+    }
+}
